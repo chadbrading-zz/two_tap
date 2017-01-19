bb4aaa79bd293f3e03e164583c67a95917df828b
Chad Brading <chadbrading@zoho.com>
2017-01-19 09:44:59 -0500

    Extract test_mode flag into config

--- lib/two_tap/http_client.ex
+++ lib/two_tap/http_client.ex
@@ -3,6 +3,7 @@ defmodule TwoTap.HTTPClient do
   @private_token Application.fetch_env!(:two_tap, :private_token)
   @confirm_purchase_callback Application.fetch_env!(:two_tap, :confirm_purchase_callback)
   @update_purchase_callback Application.fetch_env!(:two_tap, :update_purchase_callback)
+  @test_mode Application.get_env(:two_tap, :test_mode)
   @two_tap_url "https://api.twotap.com/v1.0"
 
   def request(method, endpoint, body, headers, opts \\ [], retries_left \\ 3) do
@@ -20,19 +21,19 @@ defmodule TwoTap.HTTPClient do
   end
 
   def create_cart(products) do
-    request(:post, "/cart", %{products: products, public_token: @public_token, test_mode: "dummy_data"}, [{"Content-Type", "application/json"}])
+    request(:post, "/cart", %{products: products, public_token: @public_token, test_mode: @test_mode}, [{"Content-Type", "application/json"}])
   end
 
   def get_cart_status(cart_id) do
-    request(:get, "/cart/status", [], [], %{params: %{public_token: @public_token, cart_id: cart_id, test_mode: "dummy_data"}})
+    request(:get, "/cart/status", [], [], %{params: %{public_token: @public_token, cart_id: cart_id, test_mode: @test_mode}})
   end
 
   def start_purchase(cart_id, purchase_data) do
-    request(:post, "/purchase", %{cart_id: cart_id, fields_input: purchase_data, confirm: purchase_callbacks, test_mode: "dummy_data", public_token: @public_token}, [{"Content-Type", "application/json"}])
+    request(:post, "/purchase", %{cart_id: cart_id, fields_input: purchase_data, confirm: purchase_callbacks, test_mode: @test_mode, public_token: @public_token}, [{"Content-Type", "application/json"}])
   end
 
   def confirm_purchase(purchase_id) do
-    request(:post, "/purchase/confirm", %{private_token: @private_token, purchase_id: purchase_id, test_mode: "fake_confirm"}, [{"Content-Type", "application/json"}])
+    request(:post, "/purchase/confirm", %{private_token: @private_token, purchase_id: purchase_id, test_mode: @test_mode}, [{"Content-Type", "application/json"}])
   end
 
   defp to_json(data) do
